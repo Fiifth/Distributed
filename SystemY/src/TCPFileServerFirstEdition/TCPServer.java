b903b8b4eddbfc4424b5abc4560d9d3e3638c3e3
Kwinten Schram <kwinten.schram@student.uantwerpen.be>
2015-09-29 11:56:41 +0200

    Added TCP file server first edition package and class

---
+++ SystemY/src/TCPFileServerFirstEdition/TCPServer.java
@@ -0,0 +1,54 @@
+package TCPFileServerFirstEdition;
+
+import java.io.*;
+import java.net.*;
+
+class TCPServer {
+
+   // private final static String fileToSend = "C:\\test1.txt";
+
+    public static void main(String args[]) {
+    	String fileToSend = "C:\\test1.txt";
+        while (true) {
+            ServerSocket welcomeSocket = null;
+            Socket connectionSocket = null;
+            BufferedOutputStream outToClient = null;
+
+            try {
+                welcomeSocket = new ServerSocket(3248);
+                connectionSocket = welcomeSocket.accept();
+                outToClient = new BufferedOutputStream(connectionSocket.getOutputStream());
+            } catch (IOException ex) {
+                // Do exception handling
+            	System.out.println("AJ1");
+            }
+
+            if (outToClient != null) {
+                File myFile = new File( fileToSend );
+                byte[] mybytearray = new byte[(int) myFile.length()];
+
+                FileInputStream fis = null;
+
+                try {
+                    fis = new FileInputStream(myFile);
+                } catch (FileNotFoundException ex) {
+                	System.out.println("AJ2");
+                }
+                BufferedInputStream bis = new BufferedInputStream(fis);
+
+                try {
+                    bis.read(mybytearray, 0, mybytearray.length);
+                    outToClient.write(mybytearray, 0, mybytearray.length);
+                    outToClient.flush();
+                    outToClient.close();
+                    connectionSocket.close();
+
+                    // File sent, exit the main method
+                    return;
+                } catch (IOException ex) {
+                	System.out.println("AJ3");
+                }
+            }
+        }
+    }
+}
