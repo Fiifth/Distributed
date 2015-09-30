0ab0139319d53d6d5d64c9fea6d52f7a41491f10
MatsDeMeyer <matsdemeyer@gmail.com>
2015-09-30 09:54:02 +0200

    thirdedition

--- SystemY/src/TCPFileServerThirdEdition/copy/TCPServer.java
+++ SystemY/src/TCPFileServerThirdEdition/copy/TCPServer.java
@@ -8,7 +8,7 @@ class TCPServer {
    // private final static String fileToSend = "C:\\test1.txt";
 
     public static void main(String args[]) {
-    	String fileToSend = "C:\\test.rar";
+    	
         while (true) {
             ServerSocket welcomeSocket = null;
             Socket connectionSocket = null;
@@ -16,38 +16,20 @@ class TCPServer {
 
             try {
                 welcomeSocket = new ServerSocket(3248);
-                connectionSocket = welcomeSocket.accept();
-                outToClient = new BufferedOutputStream(connectionSocket.getOutputStream());
+                System.out.println("Listening...");
+                while(true)
+                {
+                	connectionSocket = welcomeSocket.accept();
+                	                	
+                }
             } catch (IOException ex) {
                 // Do exception handling
             	System.out.println("AJ1");
             }
 
             if (outToClient != null) {
-                File myFile = new File( fileToSend );
-                byte[] mybytearray = new byte[(int) myFile.length()];
-
-                FileInputStream fis = null;
-
-                try {
-                    fis = new FileInputStream(myFile);
-                } catch (FileNotFoundException ex) {
-                	System.out.println("AJ2");
-                }
-                BufferedInputStream bis = new BufferedInputStream(fis);
-
-                try {
-                    bis.read(mybytearray, 0, mybytearray.length);
-                    outToClient.write(mybytearray, 0, mybytearray.length);
-                    outToClient.flush();
-                    outToClient.close();
-                    connectionSocket.close();
-                    // File sent, exit the main method
-                    return;
-                } catch (IOException ex) {
-                	System.out.println("AJ3");
+            	TCPThread tcp1 = new TCPThread(connectionSocket);            	
                 }
             }
         }
     }
-}
