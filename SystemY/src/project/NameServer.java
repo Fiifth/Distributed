d391732346c2e419964f0e80c997e3685e2144c2
Rens Baeyens <rensbaeyens@gmail.com>
2015-10-14 09:58:00 +0200

    <3

---
+++ SystemY/src/project/NameServer.java
@@ -0,0 +1,26 @@
+package project;
+
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
+
+
+public class NameServer extends UnicastRemoteObject implements NameServerInterface{
+	Map nodeMap = new HashMap();
+
+	public void addIP() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void rmIP() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showList() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+}
