aa0ad61178665d8a38fcab65d7aed76e89b8e654
MatsDeMeyer <matsdemeyer@gmail.com>
2015-10-14 09:28:31 +0200

    Arguments in comments toegevoegd

--- SystemY/src/project/NameServer.java
+++ SystemY/src/project/NameServer.java
@@ -2,6 +2,8 @@ package project;
 
 import java.rmi.*;
 
+//-Djava.security.policy=file:$git/Distributed/SystemY/bin/project -Djava.rmi.server.codebase=file:$git/Distributed/SystemY/bin/project
+
 public class NameServer {
 	
 	public static void main(String args[]) {
