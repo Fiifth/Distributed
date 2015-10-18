0aef2c6f186ba26327db8f391fc6b5ddab742ff8
MatsDeMeyer <matsdemeyer@gmail.com>
2015-10-18 22:36:29 +0200

    mats de meesterrmifixer

--- SystemY/src/project/NameServer.java
+++ SystemY/src/project/NameServer.java
@@ -2,12 +2,28 @@ package project;
 
 import java.net.InetAddress;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*; 
 
 
 public class NameServer extends UnicastRemoteObject implements NameServerInterface{
 	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args){
+		try{
+			LocateRegistry.createRegistry(1099);
+			NameServerInterface nameint = new NameServer();
+			Naming.rebind("//localhost/NameServer", nameint);
+			//NameServer nameserverobj = new NameServer();
+			//Naming.rebind("//localhost/NameServer", nameserverobj);
+			
+			System.out.println("NameServer is ready.");
+		}catch(Exception e){
+			System.out.println("NameServer error: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 	protected NameServer() throws RemoteException {
 		super();
--- SystemY/src/project/StartNameServer.java
+++ SystemY/src/project/StartNameServer.java
@@ -8,8 +8,11 @@ public class StartNameServer {
 	public static void main(String[] args){
 		try{
 			LocateRegistry.createRegistry(2020);
-			NameServerInterface nameint = new NameServer();
-			Naming.rebind("//localhost/NameServer", nameint);
+			//NameServerInterface nameint = new NameServer();
+			//Naming.rebind("//localhost/NameServer", nameint);
+			NameServer nameserverobj = new NameServer();
+			Naming.rebind("//localhost/NameServer", nameserverobj);
+			
 			System.out.println("NameServer is ready.");
 		}catch(Exception e){
 			System.out.println("NameServer error: " + e.getMessage());
