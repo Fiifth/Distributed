355906f3c97fe9ee5e5d9909d63f9f0102dc4d60
MatsDeMeyer <matsdemeyer@gmail.com>
2015-10-20 11:01:33 +0200

    kwinten zen file gefixt

--- SystemY/src/project/Node.java
+++ SystemY/src/project/Node.java
@@ -12,28 +12,26 @@ public class Node
 		try 
 		{	
 			InetAddress mijnIP0=InetAddress.getLocalHost();
-			String naam="Node1";
 			String mijnIP= mijnIP0.getHostAddress();
 			InetAddress group = InetAddress.getByName("228.5.6.7");
 			multiastSocket = new MulticastSocket(6789);
 			multiastSocket.joinGroup(group);
-			byte [] m1 = naam.getBytes();
- 			byte [] m2 = mijnIP.getBytes(); 			
-			DatagramPacket messageOut1 = new DatagramPacket(m1, m1.length, group, 6789);
-			DatagramPacket messageOut2 = new DatagramPacket(m2, m2.length, group, 6789);
+ 			byte [] m = mijnIP.getBytes(); 			
+			DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
 			
-			multiastSocket.send(messageOut1);	
-			multiastSocket.send(messageOut2);
+			multiastSocket.send(messageOut);
 			
 			byte[] buffer = new byte[1000];
 			
  			for(int i=0; i< 2;i++)	//receive 3 messages
 			 {		
-				// get messages from others in group
  				DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
  				multiastSocket.receive(messageIn);
- 				System.out.println("Received:" + new String(messageIn.getData()));
-  		}
+ 				String msg = new String(messageIn.getData(), messageIn.getOffset(), messageIn.getLength());
+ 				System.out.println("Received:" + msg);
+ 				//if i = 2 slaag aantal nodes op en bepaal ip adres van server
+ 				//if i = 3 afleiden wie vorige en volgende nodes zijn ofzo
+			 }
 			multiastSocket.leaveGroup(group);		
 		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
 		}catch (IOException e){System.out.println("IO: " + e.getMessage());
