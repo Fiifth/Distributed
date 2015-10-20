0563c33b87e892de5a1258cfdd9c47310787235e
Kwinten Schram <kwinten.schram@student.uantwerpen.be>
2015-10-20 09:04:44 +0200

    Node uncomment en aanpassingen

--- SystemY/src/project/Node.java
+++ SystemY/src/project/Node.java
@@ -4,22 +4,31 @@ import java.io.*;
 
 public class Node 
 {
-
+	
 	public static void main(String[] args) 
 	{
-		/*
+		
 		MulticastSocket multiastSocket =null;
 		try 
 		{
-			String msg="testFromKwinten";
+			InetAddress address = InetAddress.getLocalHost(); 		
+			String naam="Node1";
+			InetAddress mijnIP=InetAddress.getLocalHost();
 			InetAddress group = InetAddress.getByName("228.5.6.7");
 			multiastSocket = new MulticastSocket(6789);
 			multiastSocket.joinGroup(group);
- 			byte [] m = msg.getBytes();
-			DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
-			multiastSocket.send(messageOut);	
+ 			byte [] m1 = naam.getBytes();
+ 			byte [] m2 = mijnIP.getAddress();
+ 			
+			DatagramPacket messageOut1 = new DatagramPacket(m1, m1.length, group, 6789);
+			DatagramPacket messageOut2 = new DatagramPacket(m2, m2.length, group, 6789);
+			
+			multiastSocket.send(messageOut1);	
+			multiastSocket.send(messageOut2);
+			
 			byte[] buffer = new byte[1000];
- 			for(int i=0; i< 3;i++)	//receive 3 messages
+			
+ 			for(int i=0; i< 2;i++)	//receive 3 messages
 			 {		
 				// get messages from others in group
  				DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
@@ -31,7 +40,7 @@ public class Node
 		}catch (IOException e){System.out.println("IO: " + e.getMessage());
 		}finally {if(multiastSocket != null) multiastSocket.close();}
 
-*/
+
 	}
 
 }
