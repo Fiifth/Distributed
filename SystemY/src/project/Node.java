907a8b828b6627f4f0179bfbeead5c1106a0c578
Kwinten Schram <kwinten.schram@student.uantwerpen.be>
2015-10-25 02:02:12 +0200

    node code opgeslitst in verschillende functies

--- SystemY/src/project/Node.java
+++ SystemY/src/project/Node.java
@@ -4,46 +4,60 @@ import java.io.*;
 
 public class Node 
 {	
-	public String nodeNaam;
+	public static String nodeName;
 	public String prevNode;
 	public String nextNode;
-	
+	public static String nameServerIP;
+	//TODO bij ontvangen multicast nieuwe node: update next/previous node
 	public static void main(String[] args)throws Exception
 	{
-		//TODO bij ontvangen multicast nieuwe node: update next/previous node
-		MulticastSocket multicastSocket =null;
-		ServerSocket welcomeSocket = null;
-		Socket connectionSocket = null;
+		nodeName="Node2";
+		int numberOfNodes;
 		
+		sendMulticast(nodeName);
+		numberOfNodes=getNameServerRespons();
+	}
+	
+	public static void sendMulticast(String name)
+	{
+		MulticastSocket multicastSocket =null;
+		byte [] m1 = name.getBytes();
 		try 
 		{	
-			String naam="Node1";
 			InetAddress group = InetAddress.getByName("228.5.6.7");
 			multicastSocket = new MulticastSocket(6789);
 			multicastSocket.joinGroup(group);
-			byte [] m1 = naam.getBytes();
 			DatagramPacket messageOut1 = new DatagramPacket(m1, m1.length, group, 6789);
-			
 			multicastSocket.send(messageOut1);	
-			
 			multicastSocket.leaveGroup(group);		
 		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
 		}catch (IOException e){System.out.println("IO: " + e.getMessage());
 		}finally {if(multicastSocket != null) multicastSocket.close();}
-		
-		//set up TCP socket to receive IPaddress from server and # nodes
+	}
+	
+	public static int getNameServerRespons()
+	{
+		ServerSocket welcomeSocket = null;
+		Socket connectionSocket = null;
 		InetAddress serverIP;
-		String amountOfNodes;
-		welcomeSocket = new ServerSocket(6789);
-		connectionSocket = welcomeSocket.accept();
-		BufferedReader inFromNameServer = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-		amountOfNodes = inFromNameServer.readLine();
-		System.out.println("amount of Nodes: " + amountOfNodes);
-		serverIP=connectionSocket.getInetAddress();
-		String serverIPstring=serverIP.getHostAddress();
-		System.out.println("serverIP: " + serverIPstring);
-		welcomeSocket.close();
-		connectionSocket.close();
+		int nodes=0;
+		
+		try {
+			welcomeSocket = new ServerSocket(6789);
+			connectionSocket = welcomeSocket.accept();
+			welcomeSocket.close();
+			BufferedReader inFromNameServer = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			String amountOfNodes = inFromNameServer.readLine();
+			nodes=Integer.parseInt(amountOfNodes);
+			System.out.println("amount of Nodes: " + amountOfNodes);
+			serverIP=connectionSocket.getInetAddress();
+			nameServerIP=serverIP.getHostAddress();
+			System.out.println("serverIP: " + nameServerIP);
+			
+			connectionSocket.close();
+		} 
+		catch (IOException e) {e.printStackTrace();	}
+		return nodes;
 	}
 
 }
