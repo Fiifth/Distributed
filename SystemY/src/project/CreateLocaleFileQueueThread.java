a84dd5f6daef5180191afdd93dd9fd8abbd69083
Rens Baeyens <rensbaeyens@gmail.com>
2015-10-30 00:11:22 +0100

    update van queue mogelijk

--- SystemY/src/project/CreateLocaleFileQueueThread.java
+++ SystemY/src/project/CreateLocaleFileQueueThread.java
@@ -1,20 +1,25 @@
 package project;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class CreateLocaleFileQueueThread extends Thread
 {
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 		CreateLocaleFileQueueThread test = new CreateLocaleFileQueueThread();
 		test.searchFilesToAdd();
+		test.sleep(5000);
+		test.updateQueueWithNewFiles();
 	}
-	Queue<String> myQueue;
+	Queue<String> fnQueue;
+	ArrayList<String> fnList;
 	String dirToSearch;
 	public CreateLocaleFileQueueThread()
 	{
-		myQueue = new LinkedList<String>();
+		fnQueue = new LinkedList<String>();
+		fnList = new ArrayList<String>();
 		dirToSearch = "C:\\SystemYNodeFiles";
 	}
 	public void searchFilesToAdd()
@@ -24,11 +29,23 @@ public class CreateLocaleFileQueueThread extends Thread
 		for (File file: listOfFilesInDir){
 			if(file.isFile()){
 				String fileName = file.getName();
-				myQueue.add(fileName);
-				System.out.println(fileName + "\n");
+				fnQueue.add(fileName);
+				fnList.add(fileName);
 			}
-		}
-		
+		}	
+	}
+	
+	public void updateQueueWithNewFiles(){
+		File folder = new File(dirToSearch);
+		File[] listOfFilesInDir = folder.listFiles();
+		for (File file: listOfFilesInDir){
+			if(file.isFile()){
+				String fileName = file.getName();
+				if(!fnList.contains(fileName)){
+					fnQueue.add(fileName);
+				}
+			}
+		}	
 	}
 
 }
