ec7b38556f1b11e1aef148e7a51c43d99545a27b
Rens Baeyens <rensbaeyens@gmail.com>
2015-10-29 23:55:17 +0100

    CreateLocaleFileQueueThread kleir

--- SystemY/src/project/CreateLocaleFileQueueThread.java
+++ SystemY/src/project/CreateLocaleFileQueueThread.java
@@ -1,13 +1,33 @@
 package project;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CreateLocaleFileQueueThread extends Thread
 {
+	public static void main(String[] args){
+		CreateLocaleFileQueueThread test = new CreateLocaleFileQueueThread();
+		test.searchFilesToAdd();
+	}
+	Queue<String> myQueue;
+	String dirToSearch;
 	public CreateLocaleFileQueueThread()
 	{
-		
+		myQueue = new LinkedList<String>();
+		dirToSearch = "C:\\SystemYNodeFiles";
 	}
-	public void run()
+	public void searchFilesToAdd()
 	{
+		File folder = new File(dirToSearch);
+		File[] listOfFilesInDir = folder.listFiles();
+		for (File file: listOfFilesInDir){
+			if(file.isFile()){
+				String fileName = file.getName();
+				myQueue.add(fileName);
+				System.out.println(fileName + "\n");
+			}
+		}
 		
 	}
 
