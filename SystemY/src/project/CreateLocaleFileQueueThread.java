a87ef95f2e3e244be12f17b151bdfb30aba476b7
Rens Baeyens <rensbaeyens@gmail.com>
2015-10-31 23:48:40 +0100

    filewatcher https://www.youtube.com/watch?v=04seFO6BEmw

--- SystemY/src/project/CreateLocaleFileQueueThread.java
+++ SystemY/src/project/CreateLocaleFileQueueThread.java
@@ -1,21 +1,87 @@
 package project;
 
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class CreateLocaleFileQueueThread extends Thread
-{
+
+public class CreateLocaleFileQueueThread extends Thread{
+	public WatchService watcher;
+	String dirToSearch = "C:\\SystemYNodeFiles";
+	Path dir = Paths.get(dirToSearch);
 	Queue<String> fnQueue;
-	ArrayList<String> fnList;
-	String dirToSearch;
+	ArrayList<String> fnList;	
 	public CreateLocaleFileQueueThread()
 	{
 		fnQueue = new LinkedList<String>();
 		fnList = new ArrayList<String>();
 		dirToSearch = "C:\\SystemYNodeFiles";
 	}
+	/*public static void main(String args[]){
+		CreateLocaleFileQueueThread q = new CreateLocaleFileQueueThread();
+		q.run();
+	}*/
+	public void run(){
+		//vul lijst en queue met nieuwe map
+		searchFilesToAdd();
+		try {
+			setupWatchService();
+		} catch (IOException e) {}
+		while(true){
+			WatchKey key;
+			try{
+				key = watcher.take();
+			} catch(InterruptedException ex) {
+				return;
+			}
+			for(WatchEvent<?> event : key.pollEvents()){
+				//get event type
+				WatchEvent.Kind<?> kind = event.kind();
+				 // get file name
+				@SuppressWarnings("unchecked")
+				WatchEvent<Path> ev = (WatchEvent<Path>) event;
+				Path fileName = ev.context();
+				System.out.println(kind.name() + ": " + fileName);
+				 
+				if(kind == OVERFLOW){
+					continue;
+				}
+				else if(kind == ENTRY_CREATE){
+					System.out.println("new file added");
+					fnQueue.add(fileName.toString());
+					fnList.add(fileName.toString());
+				}
+				else if(kind == ENTRY_MODIFY){
+					System.out.println("file modified");
+				}
+				//TODO replicating node inlichten over removal
+				/*else if(kind == ENTRY_DELETE){
+					
+					System.out.println("file removed");
+					fnList.remove(fileName.toString());
+					
+				}*/
+				boolean valid = key.reset();
+				if(!valid){
+					break;
+				}
+			}
+		}
+	}
+	//bij startup de lijst en queue vullen me alle bestanden
 	public void searchFilesToAdd()
 	{
 		File folder = new File(dirToSearch);
@@ -29,7 +95,8 @@ public class CreateLocaleFileQueueThread extends Thread
 		}	
 	}
 	
-	public void updateQueueWithNewFiles(){
+	//waarschijnlijk nie meer nodig
+	/*public void updateQueueWithNewFiles(){
 		File folder = new File(dirToSearch);
 		File[] listOfFilesInDir = folder.listFiles();
 		for (File file: listOfFilesInDir){
@@ -40,6 +107,11 @@ public class CreateLocaleFileQueueThread extends Thread
 				}
 			}
 		}	
+	}*/
+	//start watcher
+	public void setupWatchService() throws IOException{
+		watcher = FileSystems.getDefault().newWatchService();
+		dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
 	}
 
-}
+}	
