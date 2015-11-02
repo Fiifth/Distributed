a151b65220c77c7e068b88cde302347dbd594443
fiifth <kwinten.schram@student.uantwerpen.be>
2015-11-02 16:28:15 +0100

    Laatste file die gemaakt moet worden toegevoegd met beschrijving

---
+++ SystemY/src/nodeP/CheckOwnershipThread.java
@@ -0,0 +1,8 @@
+package nodeP;
+
+public class CheckOwnershipThread extends Thread
+{
+	//TODO bij nieuwe node controleren of gerepliceerde bestanden een nieuwe eigenaar moeten hebben
+	//bij received files alles bijhouden en lijst afgaan via RMI of de ontvange files
+	//een nieuwe eigenaar hebben
+}
